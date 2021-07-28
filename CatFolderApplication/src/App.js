bc8d3cdcbae264950aa30638e02dbf71463209fd
jinn2u <bsybear623@gmail.com>
2021-07-28 23:28:34 +0900

    뒤로가기 버튼 안보이는 문제 수정

--- CatFolderApplication/src/App.js
+++ CatFolderApplication/src/App.js
@@ -8,7 +8,8 @@ export default function App($app) {
   this.state = {
     isRoot: false,
     nodes: [],
-    depth: []
+    depth: [],
+    selectedFilePath: null
   }
   const breadcrumb = new Breadcrumb({
     $app,
@@ -19,25 +20,31 @@ export default function App($app) {
     initialState: {
       isRoot: this.state.isRoot,
       nodes: this.state.nodes,
-      onClick: async (node) => {
-        try {
-          if (node.type ==='DICTIONARY') {
-            const nextNodes = await request(node.id)
-            this.setState({
-              ...this.state,
-              depth: [...this.state.depth, node],
-              nodes: nextNodes
-            })
-          } else if (node.type === 'FILE') {
-            // 이미지 보기 처리하기
-          }
-        } catch (e) {
-
+    },
+    onClick: async (node) => {
+      try {
+        if (node.type ==='DIRECTORY') {
+          const nextNodes = await request(node.id)
+          this.setState({
+            ...this.state,
+            isRoot: false,
+            depth: [...this.state.depth, node],
+            nodes: nextNodes
+          })
+        } else if (node.type === 'FILE') {
+          this.setState({
+            ...this.state,
+            isRoot: false,
+            selectedFilePath: node.filePath
+          })
         }
+      } catch (e) {
+        throw "file or directory is invalid"
       }
-    }
+    },
   })
-  this.setState = nextState => {
+
+  this.setState = (nextState) => {
     this.state = nextState
     breadcrumb.setState(this.state.depth)
     nodes.setState({
